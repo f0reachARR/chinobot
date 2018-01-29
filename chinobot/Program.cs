78bddb76dd04511287d2fa6d31102450808062e7
Peijun/ページュン <contact.zouri.music@gmail.com>
2018-01-30 00:38:02 +0900

    Correspond to image transmission

--- chinobot/Program.cs
+++ chinobot/Program.cs
@@ -22,20 +22,26 @@ namespace chinobot
         {
             if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.TextMessage)
             {
-                if (e.Message.Text == "/おはよう")
+                if (e.Message.Text == "おはよう")
                 {
                     Bot.SendTextMessageAsync(e.Message.Chat.Id, "おはようございます" + e.Message.Chat.Username + "さん");
                     Bot.SendTextMessageAsync(e.Message.Chat.Id, "今日も天気がいいですね");
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, "http://crypto.peijun.info/wp-content/uploads/2018/01/7ca47fac-s.jpg");
+
                 }
-                else if (e.Message.Text == "/おやすみ")
+                else if (e.Message.Text == "おやすみ")
+                {
                     Bot.SendTextMessageAsync(e.Message.Chat.Id, "おやすみなさい" + e.Message.Chat.Username + "さん");
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, "http://crypto.peijun.info/wp-content/uploads/2018/01/maxresdefault.jpg");
+                }
                 else
                 {
                     Bot.SendTextMessageAsync(e.Message.Chat.Id, "ごめんなさい、よくわからないです。");
-
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, "http://crypto.peijun.info/wp-content/uploads/2018/01/CezUMjHWwAAUtLq.jpg");
                 }
             }
 
         }
-    }
+
+       }
 }
