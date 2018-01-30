3dcd8adae9265294a93243c20dd65d368247d132
Peijun/ページュン <contact.zouri.music@gmail.com>
2018-01-30 22:24:21 +0900

    add omikuji

--- chinobot/Program.cs
+++ chinobot/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using System.Drawing.Imaging;
+using System.Net;
+using System.IO;
 
 namespace chinobot
 {
     class Program
     {
-        private static readonly TelegramBotClient Bot = new TelegramBotClient("523439661:AAGfyMnZdEz9jjNn36UwMMya4xtRiRxFfHU");
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("492320650:AAFcIMsiNSqhcAq_GIdK38RPju-rT-it6mU");
 
         static void Main(string[] args)
         {
@@ -33,6 +36,33 @@ namespace chinobot
                 {
                     Bot.SendTextMessageAsync(e.Message.Chat.Id, "おやすみなさい" + e.Message.Chat.Username + "さん");
                     Bot.SendTextMessageAsync(e.Message.Chat.Id, "http://crypto.peijun.info/wp-content/uploads/2018/01/maxresdefault.jpg");
+                }
+                else if (e.Message.Text == "おみくじ")
+                {
+                    System.Random r = new System.Random();
+                    int i1 = r.Next(10);
+                    //int i1 = 2; for debug
+                    if (i1 == 1)
+                    {
+                        Bot.SendTextMessageAsync(e.Message.Chat.Id, "おめでとうございます。大吉です。");
+                    }
+                    else if (i1 == 2)
+                    {
+                        /* trying send photo
+                        string url = "URL";
+                        WebClient wc = new WebClient();
+                        Stream stream = wc.OpenRead(url);
+                        Bitmap bitmap = new Bitmap(stream);
+                        stream.Close();
+                        */
+
+                        Bot.SendTextMessageAsync(e.Message.Chat.Id, "大凶です。元気出してお兄ちゃん。");
+                        Bot.SendTextMessageAsync(e.Message.Chat.Id, "https://wp.me/a9uW6J-1C");
+                    }
+                    else
+                    {
+                        Bot.SendTextMessageAsync(e.Message.Chat.Id, "中吉です。");
+                    }
                 }
                 else
                 {
